74008a4ee99377d3dcd3e7d04a9e7e454464af1b
Gonzaloagustinn <gonzaloagustinsal@gmail.com>
2026-09-01 00:01:35 -0300

    creación del test clock

---
+++ tests/Clock.test.ts
@@ -0,0 +1,65 @@
+import { describe, expect, test, vi, beforeEach, afterEach } from "vitest";
+import { Clock } from "../Clock"; 
+
+describe("Clock", () => {
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+    vi.useRealTimers();
+  });
+
+  test("Debe ejecutar la funcion onTick despues del tiempo establecido", () => {
+   
+    const onTickMock = vi.fn();
+    
+  
+    const clock = new Clock(1000, onTickMock);
+    clock.start();
+
+   
+    expect(onTickMock).not.toHaveBeenCalled();
+
+    vi.advanceTimersByTime(1000);
+
+  
+    expect(onTickMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("Debe dejar de ejecutar onTick cuando se llama a stop()", () => {
+    const onTickMock = vi.fn();
+    const clock = new Clock(1000, onTickMock);
+    
+    clock.start();
+    vi.advanceTimersByTime(1000);
+    expect(onTickMock).toHaveBeenCalledTimes(1); 
+
+   
+    clock.stop();
+
+  
+    vi.advanceTimersByTime(1000);
+
+   
+    expect(onTickMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("Debe cambiar la velocidad correctamente con setSpeed()", () => {
+    const onTickMock = vi.fn();
+    const clock = new Clock(1000, onTickMock);
+    
+    clock.start();
+    
+    
+    clock.setSpeed(500);
+
+  
+    vi.advanceTimersByTime(500);
+
+    expect(onTickMock).toHaveBeenCalledTimes(1);
+  });
+});
